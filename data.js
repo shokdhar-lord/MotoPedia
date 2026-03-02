89b88b3eb2f526e732f85cac9a9676f7d4fca3dd
shokdhar-lord <vinayrockadda2020@gmail.com>
2026-03-01 19:47:50 -0800

    Refactor bikes data structure
    Removed duplicate entries and cleaned up formatting.

--- data.js
+++ data.js
@@ -71,13 +71,7 @@ const bikes = [
     type: "Cruiser",
     image: "images/iron883.jfif"
 },
-{
-    brand: "Harley-Davidson",
-    model: "Iron 883",
-    year: 2009,
-    cc: 883,
-    type: "Cruiser",
-    image: "images/iron883.jfif"
-},
+
 
 ];
+
