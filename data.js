7cdbd9d3832c8da32495f707960d5ba6890c7158
shokdhar-lord <vinayrockadda2020@gmail.com>
2026-03-04 22:31:03 +0530

    Update data.js

--- data.js
+++ data.js
@@ -71,7 +71,158 @@ const bikes = [
     type: "Cruiser",
     image: "images/iron883.jfif"
 },
-
+{
+  brand: "Aprilia",
+  model: "RSV4",
+  year: 2023,
+  cc: 1099,
+  type: "Sports",
+  image: "images/aprilia-rsv4.jpg"
+},
+{
+  brand: "MV Agusta",
+  model: "F3 800",
+  year: 2022,
+  cc: 798,
+  type: "Sports",
+  image: "images/mv-f3.jpg"
+},
+{
+  brand: "Triumph",
+  model: "Daytona 765",
+  year: 2023,
+  cc: 765,
+  type: "Sports",
+  image: "images/daytona765.jpg"
+},
+{
+  brand: "Suzuki",
+  model: "GSX-R750",
+  year: 2023,
+  cc: 750,
+  type: "Sports",
+  image: "images/gsxr750.jpg"
+}
+{
+  brand: "Triumph",
+  model: "Tiger 900",
+  year: 2023,
+  cc: 888,
+  type: "Adventure",
+  image: "images/tiger900.jpg"
+},
+{
+  brand: "KTM",
+  model: "1290 Super Adventure",
+  year: 2023,
+  cc: 1301,
+  type: "Adventure",
+  image: "images/ktm1290.jpg"
+},
+{
+  brand: "Honda",
+  model: "Africa Twin",
+  year: 2023,
+  cc: 1084,
+  type: "Adventure",
+  image: "images/africatwin.jpg"
+},
+{
+  brand: "Ducati",
+  model: "Multistrada V4",
+  year: 2023,
+  cc: 1158,
+  type: "Adventure",
+  image: "images/multistrada.jpg"
+}
+{
+  brand: "Indian",
+  model: "Scout Bobber",
+  year: 2023,
+  cc: 1133,
+  type: "Cruiser",
+  image: "images/scoutbobber.jpg"
+},
+{
+  brand: "Harley-Davidson",
+  model: "Street Glide",
+  year: 2023,
+  cc: 1746,
+  type: "Cruiser",
+  image: "images/streetglide.jpg"
+},
+{
+  brand: "Kawasaki",
+  model: "Vulcan S",
+  year: 2023,
+  cc: 649,
+  type: "Cruiser",
+  image: "images/vulcans.jpg"
+},
+{
+  brand: "Royal Enfield",
+  model: "Super Meteor 650",
+  year: 2023,
+  cc: 648,
+  type: "Cruiser",
+  image: "images/supermeteor.jpg"
+}
+{
+  brand: "Yamaha",
+  model: "MT-09",
+  year: 2023,
+  cc: 890,
+  type: "Naked",
+  image: "images/mt09.jpg"
+},
+{
+  brand: "Honda",
+  model: "CB650R",
+  year: 2023,
+  cc: 649,
+  type: "Naked",
+  image: "images/cb650r.jpg"
+},
+{
+  brand: "Kawasaki",
+  model: "Z900",
+  year: 2023,
+  cc: 948,
+  type: "Naked",
+  image: "images/z900.jpg"
+},
+{
+  brand: "Ducati",
+  model: "Monster",
+  year: 2023,
+  cc: 937,
+  type: "Naked",
+  image: "images/monster.jpg"
+}
+{
+  brand: "Zero",
+  model: "SR/F",
+  year: 2023,
+  cc: 0,
+  type: "Electric",
+  image: "images/zerosrf.jpg"
+},
+{
+  brand: "LiveWire",
+  model: "One",
+  year: 2023,
+  cc: 0,
+  type: "Electric",
+  image: "images/livewireone.jpg"
+},
+{
+  brand: "Energica",
+  model: "Ego",
+  year: 2023,
+  cc: 0,
+  type: "Electric",
+  image: "images/energicaego.jpg"
+}
 
 ];
 
