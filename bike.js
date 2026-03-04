8ec6b582e9056a61af67ba6aade0c8037c2a5d23
shokdhar-lord <vinayrockadda2020@gmail.com>
2026-03-04 22:24:27 +0530

    Add initial bike data to bike.js
    Initialized bike.js with a list of bike objects.

---
+++ bike.js
@@ -0,0 +1,28 @@
+const bikes = [
+  {
+    brand: "Yamaha",
+    model: "R1",
+    year: 2023,
+    cc: 998,
+    type: "Sports",
+    image: "images/r1.jpg"
+  },
+  {
+    brand: "Honda",
+    model: "CBR1000RR",
+    year: 2022,
+    cc: 1000,
+    type: "Sports",
+    image: "images/cbr1000rr.jpg"
+  },
+  {
+    brand: "BMW",
+    model: "GS 1250",
+    year: 2023,
+    cc: 1254,
+    type: "Adventure",
+    image: "images/gs1250.jpg"
+  }
+
+  // 🔥 Add all 250 bikes here
+];
